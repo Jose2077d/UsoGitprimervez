3b8c27f38b0dd6c916597e8f40b97e3ca095e93d
José Daniel Monge <Jos Daniel Monge@DESKTOP-ISLK8PO>
2023-06-26 21:42:04 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿primervezusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Usogit1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
